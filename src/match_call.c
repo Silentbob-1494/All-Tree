ce63e05b817297fd8de5208fba8dcbb3cee1a95e
Silentbob-1494 <140295824+Silentbob-1494@users.noreply.github.com>
2023-08-24 20:58:33 -0500

    no more annoying match calls

--- src/match_call.c
+++ src/match_call.c
@@ -1108,8 +1108,8 @@ static bool32 SelectMatchCallTrainer(void)
         return FALSE;
 
     matchCallId = GetTrainerMatchCallId(sMatchCallState.trainerId);
-    if (GetRematchTrainerLocation(matchCallId) == gMapHeader.regionMapSectionId && !TrainerIsEligibleForRematch(matchCallId))
-        return FALSE;
+    if (!((TrainerIsEligibleForRematch(matchCallId) && GetRematchTrainerLocation(matchCallId) == gMapHeader.regionMapSectionId)
+    || ShouldTrainerRequestBattle(matchCallId)))        return FALSE;
 
     return TRUE;
 }
