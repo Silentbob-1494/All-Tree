8c9be94905abb216f167353f7a62fc0dd39bcb31
Silentbob-1494 <140295824+Silentbob-1494@users.noreply.github.com>
2023-09-03 00:11:36 -0500

    Fixed some stuff

--- include/event_object_movement.h
+++ include/event_object_movement.h
@@ -21,6 +21,11 @@ enum {
     PALSLOT_NPC_4_REFLECTION,
     PALSLOT_NPC_SPECIAL,
     PALSLOT_NPC_SPECIAL_REFLECTION,
+    PALSLOT_WALLACE_REF,
+    PALSLOT_WALLY,
+    PALSLOT_KYOGRE,
+    PALSLOT_GROUDON,
+    PALSLOT_RAYQUAZA,
     OBJ_PALSLOT_COUNT
     // the remaining sprite palette slots (12-15) are used by field effects, the interface, etc.
 };
